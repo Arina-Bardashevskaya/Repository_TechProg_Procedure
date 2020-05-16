b5a327e6eaa7637c52d8480a89ed269f760ee2dc
Арина Чумакова <arishiklike@gmail.com>
2020-05-16 22:52:06 +0700

    Добавление нового класса Numeric

--- Procedure/code_atd.h
+++ Procedure/code_atd.h
@@ -1,17 +1,19 @@
 #ifndef __code_atd__
 #define __code_atd__
 #include "zamena_atd.h"				
-#include "cezar_atd.h"		
+#include "cezar_atd.h"	
+#include "numeric_atd.h"
 
 namespace simple_codes
 {
 	struct code
 	{
-		enum key { ZAMENA, CEZAR };
+		enum key { ZAMENA, CEZAR, NUMERIC };
 		key k;
 		union {
 			zamena r;
 			cezar t;
+			numeric b;
 		};
 		char message[20];
 
